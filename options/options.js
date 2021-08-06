add2d0f338d6859108a64d27d2c5c20a6cb8eb79
Crowdbotics <team@crowdbotics.com>
2021-08-06 18:24:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_credit_29576Identifier",
+  url: "https://rapid_credit_29576Identifier.botics.co",
+  api: "https://rapid_credit_29576Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
